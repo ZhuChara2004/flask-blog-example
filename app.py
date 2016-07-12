4571bc464e158f5b1853e2bb9b3428de81427768
zhuchara <zhuchara@gmail.com>
2016-07-12 11:31:33 +0300

    delete action added

--- app.py
+++ app.py
@@ -6,19 +6,19 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     from models import Post
     return render_template('index.html', posts=Post.query.order_by(Post.created_at.desc()).all())
 
 
-@app.route('/posts/<id>')
+@app.route('/posts/<id>', methods=['GET'])
 def show(id):
     from models import Post
     return render_template('show.html', post=Post.query.get(id))
 
 
-@app.route('/posts/new')
+@app.route('/posts/new', methods=['GET'])
 def new():
     return render_template('new.html')
 
@@ -33,7 +33,7 @@ def create():
     return render_template('index.html', posts=Post.query.order_by(Post.created_at.desc()).all())
 
 
-@app.route('/posts/<id>/edit')
+@app.route('/posts/<id>/edit', methods=['GET'])
 def edit(id):
     from models import Post
     return render_template('edit.html', post=Post.query.get(id))
@@ -49,6 +49,16 @@ def update(id):
     db.session.add(post)
     db.session.commit()
     return render_template('show.html', post=post)
+
+
+@app.route('/posts/<id>/delete', methods=['GET'])
+def delete(id):
+    from models import Post
+    from app import db
+    post = Post.query.get(id)
+    db.session.delete(post)
+    db.session.commit()
+    return render_template('index.html', posts=Post.query.order_by(Post.created_at.desc()).all())
 
 
 if __name__ == '__main__':
